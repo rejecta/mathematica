c9bfc80f65d5b2961907f01c7e7daf26f4e90942
Jaska <jlaska@gmail.com>
2014-12-18 18:53:59 -0800

    Comment-sanitized code for Volume1 No1.  Added build.py and clean.py for easy building of full volume and all independent articles.

---
+++ Journal/Volume1/Number1/build.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import os
+
+def pdflatex(filepath):
+    os.system('pdflatex {0} > /dev/null'.format(filepath))
+
+def plaintex(filepath):
+    os.system('pdftex {0} > /dev/null'.format(filepath))
+
+def bibtex(filepath):
+    bibfilepath = '/'.join(filepath.split('.')[:-1])
+    os.system('bibtex {0} > /dev/null'.format(bibfilepath))
+
+def latexwithbib(filepath):
+    pdflatex(filepath)
+    bibtex(filepath)
+    pdflatex(filepath)
+    pdflatex(filepath)
+
+class temp_cd():
+    def __init__(self, temp_dir):
+        self._temp_dir = temp_dir
+        self._return_dir = os.path.dirname(os.path.realpath(__file__))
+    def __enter__(self):
+        os.chdir(self._temp_dir)
+    def __exit__(self, type, value, traceback):
+        os.chdir(self._return_dir)
+
+
+# Compile cover letter
+filename = 'coverletter25june2009.tex'
+doc_dir = 'author_source/coverletter/'
+print 'Compiling', filename, '...'
+with temp_cd(doc_dir):
+    latexwithbib(filename)
+
+# Compile each issue
+src_dir = 'author_source'
+for author_dir in os.listdir(src_dir):
+    if author_dir == '.':
+        continue
+
+    doc_types = ['openletter', 'article']
+    for doc_type in doc_types:
+        doc_dir = os.path.join(src_dir, author_dir, doc_type)
+        if not os.path.isdir(doc_dir):
+            continue
+
+        for filename in os.listdir(doc_dir):
+            if filename.endswith('.tex'):
+                print 'Compiling', doc_dir, filename, '...'
+                with temp_cd(doc_dir):
+                    latexwithbib(filename)
+            if filename.endswith('.plaintex'):
+                print 'Compiling', doc_dir, filename, '...'
+                with temp_cd(doc_dir):
+                    plaintex(filename)
+
+# Build full journal volume
+filename = 'RejectaMathematicaVol1No1.tex'
+latexwithbib(filename)
+
---
+++ Journal/Volume1/Number1/clean.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import os
+
+# DO NOT CLEAN PDFS
+
+# Clean root source
+for doc_filename in os.listdir('.'):
+    if doc_filename.endswith('.aux') or\
+        doc_filename.endswith('.bbl') or\
+        doc_filename.endswith('.blg') or\
+        doc_filename.endswith('.out') or\
+        doc_filename == '.DS_Store' or\
+        doc_filename.endswith('.log'):
+
+        os.remove(os.path.join('.', doc_filename))
+
+for doc_filename in os.listdir('author_source/coverletter'):
+    if doc_filename.endswith('.aux') or\
+        doc_filename.endswith('.bbl') or\
+        doc_filename.endswith('.blg') or\
+        doc_filename.endswith('.out') or\
+        doc_filename == '.DS_Store' or\
+        doc_filename.endswith('.log'):
+
+        os.remove(os.path.join('author_source/coverletter', doc_filename))
+
+
+# Clean tex aux files from each issue
+src_dir = 'author_source'
+for author_dir in os.listdir(src_dir):
+    if author_dir.startswith('.'):
+        continue
+
+    doc_types = ['openletter', 'article']
+    for doc_type in doc_types:
+        doc_dir = os.path.join(src_dir, author_dir, doc_type)
+        if not os.path.isdir(doc_dir):
+            continue
+
+        for doc_filename in os.listdir(doc_dir):
+            if doc_filename.endswith('.aux') or\
+                doc_filename.endswith('.bbl') or\
+                doc_filename.endswith('.blg') or\
+                doc_filename.endswith('.out') or\
+                doc_filename == '.DS_Store' or\
+                doc_filename.endswith('.log'):
+
+                os.remove(os.path.join(doc_dir, doc_filename))
